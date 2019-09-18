ee64a0ca146a91f0878319b8cdf285a54748c2a2
Anthony <sean.a.protho1@gmail.com>
2019-09-17 21:04:48 -0700

    Reformated code and added style

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
-    params = {"Key": "AAHE3B82O96Z", "q": user_input, "limit": 10}
+    params = {"Key": "AAHE3B82O96Z", "q": user_input, "limit": 15}
 
     # TODO: Make an API call to Tenor using the 'requests' library. For 
     # reference on how to use Tenor, see: 
